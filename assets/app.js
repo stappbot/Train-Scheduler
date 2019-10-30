29e8a83b802db29280eb5fa83d59d416c6ca2999
Jessica Stapp <stapp9@gmail.com>
2019-10-30 15:05:13 -0700

    append-child to firebase database to track train inputs

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@ var config = {
   messagingSenderId: "591108823841",
   appId: "1:591108823841:web:07093c4d74c82d7264fc60"
 };
-//initialize Firbase
+//initialize Firebase
 firebase.initializeApp(config);
 
 var trainName = "";
@@ -18,7 +18,8 @@ var nextArrival = "";
 var minutesAway = 0;
 
 //click event
-$("#add-train").on("click", function() {
+$("#add-train").on("click", function(event) {
+  event.preventDefault();
   trainName = $("#trainInput")
     .val()
     .trim();
@@ -31,6 +32,7 @@ $("#add-train").on("click", function() {
   frequencyMinutes = $("#frequencyInput")
     .val()
     .trim();
+  console.log("Click event occurred");
 
   //next arrival and minutes away are to be calculated elsewhere
 
@@ -38,7 +40,7 @@ $("#add-train").on("click", function() {
   firebase
     .database()
     .ref()
-    .set({
+    .push({
       trainName: trainName,
       destination: destination,
       firstTrainTime: firstTrainTime,
@@ -49,9 +51,28 @@ $("#add-train").on("click", function() {
 firebase
   .database()
   .ref()
-  .on("value", function(snapshot) {
-    $("#trainInput").html(snapshot.val().trainName);
-    $("#destinationInput").html(snapshot.val().destination);
-    $("#firstTrainTimeInput").html(snapshot.val().firstTrainTime);
-    $("#frequencyInput").html(snapshot.val().frequencyMinutes);
+  .on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+    //below is not working
+    // let train1 = snapshot.trainName;
+    // let destination1 = snapshot.destination;
+    // let frequency1 = snapshot.frequencyMinutes;
+    // let firstTrain1 = snapshot.firstTrainTime;
+    // console.log(train1);
+    // console.log(destination1);
+    // console.log(frequency1);
+    // console.log(firstTrain1);
+
+    // $("#trainInput").html(snapshot.val().trainName);
+    // $("#destinationInput").html(snapshot.val().destination);
+    // $("#firstTrainTimeInput").html(snapshot.val().firstTrainTime);
+    // $("#frequencyInput").html(snapshot.val().frequencyMinutes);
   });
+
+// moment.js
+// const diffBetweenCurrentTimeAndFirstTrainTime = currentTime - firstTrainTime
+// const timeRemainder = diffBetweenCurrentTimeAndFirstTrainTime % trainFrequency
+// // this is your minutes away value
+// const minutesAway = trainFrequency - timeRemainder
+// // next train time
+// const nextTrainTime = minutesAway + currentTime
